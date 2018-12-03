20e10a17127dc798d0b12aae2aef4a6df6bd8078
Pablo Arques Manjón <reydelmambo17@gmail.com>
2018-12-03 15:48:19 +0100

    Update confred.sh

--- confred.sh
+++ confred.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/bash	
 source ./datos.cfg
 v=/etc/network/interfaces
-echo auto lo > $v
-echo iface lo inet loopback >> $v
-echo auto eth0 >> $v
-echo iface eth0 inet static >> $v
-echo address $IP >> $v
-echo netmask $MASQ >> $v
-echo gateway $PASARELA >> $v
-echo nameserver $PASARELA > /etc/resolv.conf
+echo "auto lo" > $v
+echo "iface lo inet loopback" >> $v
+echo "auto eth0" >> $v
+echo "	iface eth0 inet static" >> $v
+echo "	address $IP" >> $v
+echo "	netmask $MASQ" >> $v
+echo "	gateway $PASARELA" >> $v
+echo "	nameserver $PASARELA" > /etc/resolv.conf
 systemctl restart networking.service
