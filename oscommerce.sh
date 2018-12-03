859bc6e33374f3f8fe699c212b7449c8b95dab0a
Pablo Arques Manjón <reydelmambo17@gmail.com>
2018-12-03 15:42:24 +0100

    Update oscommerce.sh

--- oscommerce.sh
+++ oscommerce.sh
@@ -22,17 +22,16 @@ do
 		if [ -f /etc/apache2/sites-available/$a.conf ]; then
 			echo "El alojamiento exisite, se procedeera  la creación del e-commerce"
 			cp packages/oscommerce.tar.gz /var/www/$a
-			
 			read -p "¿En que lugar de su página quieres que este el e-commerce?: (Escriba la ruta, si lo quiere en la raíz del sitio web, simplemente pulse intro): [/] " ruta
 			tar xf /var/www/$a/oscommerce.tar.gz -C /var/www/$a/public_html/$ruta
 			chmod 777 /var/www/$a/public_html/$ruta/includes/configure.php
 			chmod 777 /var/www/$a/public_html/$ruta/admin/includes/configure.php
 			PASSWDDB="$(openssl rand -base64 12)"
 			MAINDB=${a//[^a-zA-Z0-9]/_}
-		    mysql -e "CREATE DATABASE ${MAINDB}_oscom /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-		    mysql -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
-		    mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}_oscom.* TO '${MAINDB}'@'localhost';"
-		    mysql -e "FLUSH PRIVILEGES;"
+		    	mysql -e "CREATE DATABASE ${MAINDB}_oscom /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+		    	mysql -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+		    	mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}_oscom.* TO '${MAINDB}'@'localhost';"
+		    	mysql -e "FLUSH PRIVILEGES;"
 			clear
 			echo "Accede a www.$a para continuar con la instalación"
 			echo "Necesitaras esta información para completar la instalación"
